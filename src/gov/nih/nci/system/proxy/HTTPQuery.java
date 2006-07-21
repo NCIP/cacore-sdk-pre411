a086dd95b8ba261455f4e0f0388a12f795590818
Satish Patel <satish79>
2006-07-21 15:41:35 +0000

    Replaced with version 1.1
    SVN-Revision: 67

--- src/gov/nih/nci/system/proxy/HTTPQuery.java
+++ src/gov/nih/nci/system/proxy/HTTPQuery.java
@@ -127,7 +127,7 @@ public class HTTPQuery extends HttpServlet{
             if(httpUtils.getPageNumber()!= null){
                 pageNumber = Integer.parseInt(httpUtils.getPageNumber());
             } 
-            httpUtils.setServletName(request.getRequestURI().toString());
+            httpUtils.setServletName(request.getRequestURL().toString());
             
             if(httpUtils.getPageSize() != null){
                 pageSize = Integer.parseInt(httpUtils.getPageSize());           
