4f7aa7d8905621654bb0ac18d94745411b508344
Daniel Dumitru <ddumitru>
2006-09-07 22:41:28 +0000

    Refactored to remove query() method, which should be placed in the DAO instance, and not the DAO Factory.
    SVN-Revision: 167

--- src/gov/nih/nci/system/dao/ORMDAOFactory.java
+++ src/gov/nih/nci/system/dao/ORMDAOFactory.java
@@ -38,7 +38,7 @@ import org.apache.log4j.Logger;
 public class ORMDAOFactory extends DAOFactory {
 	
 		private static Logger log = Logger.getLogger(ORMDAOFactory.class);
-		private gov.nih.nci.system.dao.impl.orm.ORMDAOImpl ormImpl;
+		private static gov.nih.nci.system.dao.impl.orm.ORMDAOImpl ormImpl = new ORMDAOImpl();
 		/**
 		 * Default Constructor
 		 *
@@ -46,35 +46,9 @@ public class ORMDAOFactory extends DAOFactory {
 		public ORMDAOFactory()
 		{
 			super();
-			ormImpl = new ORMDAOImpl();
-			//hard coded
 		}
 		
-		/**
-		 * Return the resultset of the query embedded in an object of gov.nih.nci.common.net.Response
-		 * @param request - a gov.nih.nci.common.net.Request object passed from client 
-		 * @return an object of gov.nih.nci.common.net.Response that contains the query resultset
-		 * @throws DAOException
-		 */    
-	    public Response query(Request request) throws DAOException
-		{
-	    	Response response;
-			try
-			{
-				response = ormImpl.query(request);
-			}
-			catch (DAOException e)
-			{
-				log.error(e.getMessage());
-				throw new DAOException(e.getMessage());
-			}
-
-			catch (Exception e)
-			{
-				log.error(e.getMessage());
-				throw new DAOException("Exception in the ORMDAOFactory "+e);
-			}
-
-		return response;
+		public DAO getDaoInstance() {
+			return ormImpl;
 		}
 }
