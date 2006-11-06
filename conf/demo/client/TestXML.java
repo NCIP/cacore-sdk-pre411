603b7f7ffc961bc06b9945cd8615f1d6a2911d68
Satish Patel <satish79>
2006-11-06 18:55:30 +0000

    JDK 1.4 compliance
    SVN-Revision: 309

--- conf/demo/client/TestXML.java
+++ conf/demo/client/TestXML.java
@@ -78,7 +78,7 @@ ApplicationService appService = ApplicationServiceProvider.getRemoteInstance(
 try {
 	System.out.println("Scenario 1: Retrieving a Gene based on a Gene id.");
 	Gene gene = new Gene();
-	gene.setId(Long.valueOf(2));
+	gene.setId(new Long("2"));
 
 	try {
 		XMLUtility myUtil = new XMLUtility();
