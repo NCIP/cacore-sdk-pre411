8f562f37efe8479e5fea735a4c2e5da120336b43
Daniel Dumitru <ddumitru>
2006-11-16 16:03:01 +0000

    Changes to the build process to download, install, and deploy to the latest 4.1.x Tomcat and MySQL versions, per the SCM Technology Stack spreadsheet.
    SVN-Revision: 337

--- conf/database-scripts/create_user.sql
+++
@@ -1,4 +0,0 @@
-grant all privileges on cacore.* to 'cabiouser'@'localhost' identified by 'cabio' with grant option;
-grant all privileges on cacore.* to 'cabiouser'@'localhost' identified by 'cabio' with grant option;
-grant all privileges on cacore.* to 'cabiouser'@'*' identified by 'cabio' with grant option;
-grant all privileges on cacore.* to 'cabiouser'@'%' identified by 'cabio' with grant option;
