e66fb674c2e475a3b7c85f40dfaa2409120b0187
Kunal Modi <modik@mail.nih.gov>
2006-11-17 19:41:15 +0000

    Corrected bug to accept attributes from a header element instead of header directly
    SVN-Revision: 348

--- src/gov/nih/nci/system/webservice/CSMSOAPHandler.java
+++ src/gov/nih/nci/system/webservice/CSMSOAPHandler.java
@@ -1,10 +1,14 @@
 package gov.nih.nci.system.webservice;
 
+import java.util.Iterator;
+
 import gov.nih.nci.common.util.ClientInfo;
 import gov.nih.nci.common.util.ClientInfoThreadVariable;
 import gov.nih.nci.common.util.Constant;
 import gov.nih.nci.system.server.mgmt.SecurityEnabler;
 
+import javax.wsdl.extensions.soap.SOAPHeaderFault;
+import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPHeader;
 
@@ -13,6 +17,7 @@ import org.apache.axis.Message;
 import org.apache.axis.MessageContext;
 import org.apache.axis.handlers.BasicHandler;
 import org.apache.axis.message.SOAPEnvelope;
+import org.apache.axis.message.SOAPHeaderElement;
 
 
 public class CSMSOAPHandler extends BasicHandler
@@ -39,16 +44,31 @@ public class CSMSOAPHandler extends BasicHandler
 			{
 				e.printStackTrace();
 			}
-			String userName = (String)header.getAttribute("username");
-			String password = (String)header.getAttribute("password");
-	
-			ClientInfo clientInfo = new ClientInfo();
-			clientInfo.setUserName(userName);
-			clientInfo.setPassword(password);		
-			ClientInfoThreadVariable.setClientInfo(clientInfo);
-			
+			Iterator iterator = header.examineAllHeaderElements();
+			SOAPHeaderElement headerElement ;
+			while (iterator.hasNext())
+			{
+				headerElement = (SOAPHeaderElement) iterator.next();
+				if (headerElement.getNodeName().equals("csm:CSMSecurityHeader"));
+				{
+					String username = null;
+					String password = null;
+					Iterator childElementsIterator = headerElement.getChildElements();
+					while (childElementsIterator.hasNext())
+					{
+						SOAPElement element = (SOAPElement)childElementsIterator.next();
+						if (element.getNodeName().equals("csm:username"))
+							username = element.getValue();
+						if (element.getNodeName().equals("csm:password"))
+							password = element.getValue();
+					}
+					ClientInfo clientInfo = new ClientInfo();
+					clientInfo.setUserName(username);
+					clientInfo.setPassword(password);
+					ClientInfoThreadVariable.setClientInfo(clientInfo);
+				}
+			}
 			return;
 		}
 	}
-
 }
