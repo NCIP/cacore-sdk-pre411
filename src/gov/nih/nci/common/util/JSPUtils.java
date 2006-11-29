73904346476b6559352b0f10e14d0e2aa129e4a9
Satish Patel <satish79>
2006-11-29 22:06:46 +0000

    defect fix for drop down menu throwing null pointer exception
    SVN-Revision: 393

--- src/gov/nih/nci/common/util/JSPUtils.java
+++ src/gov/nih/nci/common/util/JSPUtils.java
@@ -284,7 +284,12 @@ public ArrayList getAssociations(String className) throws Exception{
            if(!field.getType().isPrimitive()){
                if(fieldName.endsWith("Collection") || (type.startsWith("java") && type.endsWith("Collection"))){
                    String roleClassName = null;
-                   String beanName = fieldName.substring(0, fieldName.lastIndexOf("Collection"));
+                   String beanName;
+                   if(fieldName.endsWith("Collection")){
+                       beanName = fieldName.substring(0, fieldName.lastIndexOf("Collection"));
+                   }else{
+                       beanName = fieldName;
+                   }   
                    roleClassName = locateClass(beanName, packageName);
                    if(roleClassName != null){
                        roleNames.add(roleClassName);
