834365699b64d8fef4eca25fc0b695843200f5bd
Steve Hunter <hunterst@mail.nih.gov>
2007-03-05 20:05:20 +0000

    Adding QA related documents including Deliverables and Reference Materials
    SVN-Revision: 508

---
+++ Project/SDK_v3.2.1_4.0/QA/Reference Materials/Configuration files/DataPrimingOracle.sql
@@ -0,0 +1,67 @@
+
+insert into csm_application(APPLICATION_ID, APPLICATION_NAME,APPLICATION_DESCRIPTION,DECLARATIVE_FLAG,ACTIVE_FLAG,UPDATE_DATE)
+values (1, 'csmupt','UPT Super Admin Application',0,0,sysdate);
+select CSM_APPLICATI_APPLICATION__SEQ.nextval from dual;
+
+COMMIT;
+insert into csm_user (USER_ID, LOGIN_NAME,FIRST_NAME,LAST_NAME,PASSWORD,UPDATE_DATE)
+values (1, 'SuperAdmin','Super','Admin','zJPWCwDeSgG8j2uyHEABIQ==',sysdate);
+select CSM_USER_USER_ID_SEQ.nextval from dual;
+ 
+COMMIT;
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(1, 'csmupt','UPT Super Admin Application','csmupt',1,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+insert into csm_user_pe(USER_PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_ID,USER_ID,UPDATE_DATE)
+values(1,1,1,sysdate);
+select CSM_USER_PE_USER_PROTECTIO_SEQ.nextval from dual;
+
+COMMIT;
+INSERT INTO csm_application(APPLICATION_ID,APPLICATION_NAME,APPLICATION_DESCRIPTION,DECLARATIVE_FLAG,ACTIVE_FLAG,UPDATE_DATE)
+VALUES (2,'sdk','sdk',0,0,sysdate);
+select CSM_APPLICATI_APPLICATION__SEQ.nextval from dual;
+
+COMMIT;
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(2, 'sdk','sdk Application','sdk',1,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+INSERT INTO csm_privilege (privilege_id, privilege_name, privilege_description, update_date)
+VALUES(1,'CREATE','This privilege grants permission to a user to create an entity. This entity can be an object, a database entry, or a resource such as a network connection', sysdate);
+SELECT CSM_PRIVILEGE_PRIVILEGE_ID_SEQ.nextval FROM dual;
+
+COMMIT;
+INSERT INTO csm_privilege (privilege_id, privilege_name, privilege_description, update_date)
+VALUES(2,'ACCESS','This privilege allows a user to access a particular resource.  ', sysdate);
+SELECT CSM_PRIVILEGE_PRIVILEGE_ID_SEQ.nextval FROM dual;
+
+COMMIT;
+INSERT INTO csm_privilege (privilege_id, privilege_name, privilege_description, update_date)
+VALUES(3,'READ','This privilege permits the user to read data from a file, URL, socket, database, or an object. ', sysdate);
+SELECT CSM_PRIVILEGE_PRIVILEGE_ID_SEQ.nextval FROM dual;
+
+COMMIT;
+INSERT INTO csm_privilege (privilege_id, privilege_name, privilege_description, update_date)
+VALUES(4,'WRITE','This privilege allows a user to write data to a file, URL, socket, database, or object. ', sysdate);
+SELECT CSM_PRIVILEGE_PRIVILEGE_ID_SEQ.nextval FROM dual;
+
+COMMIT;
+INSERT INTO csm_privilege (privilege_id, privilege_name, privilege_description, update_date)
+VALUES(5,'UPDATE','This privilege grants permission at an entity level and signifies that the user is allowed to update and modify data for a particular entity.', sysdate);
+SELECT CSM_PRIVILEGE_PRIVILEGE_ID_SEQ.nextval FROM dual;
+
+COMMIT;
+INSERT INTO csm_privilege (privilege_id, privilege_name, privilege_description, update_date)
+VALUES(6,'DELETE','This privilege permits a user to delete a logical entity.', sysdate);
+SELECT CSM_PRIVILEGE_PRIVILEGE_ID_SEQ.nextval FROM dual;
+
+COMMIT;
+INSERT INTO csm_privilege (privilege_id, privilege_name, privilege_description, update_date)
+VALUES(7,'EXECUTE','This privilege allows a user to execute a particular resource.', sysdate);
+SELECT CSM_PRIVILEGE_PRIVILEGE_ID_SEQ.nextval FROM dual;
+
+COMMIT;
---
+++ Project/SDK_v3.2.1_4.0/QA/Reference Materials/Configuration files/InsertProtectionElements.sql
@@ -0,0 +1,300 @@
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(3, 'gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Bank','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Bank','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Bank',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(4, 'gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Cash','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Cash','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Cash',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(5, 'gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Credit','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Credit','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Credit',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(6, 'gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Payment','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Payment','gov.nih.nci.cacoresdk.domain.inheritance.childwithassociation.Payment',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(7, 'gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.GraduateStudent','gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.GraduateStudent','gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.GraduateStudent',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(8, 'gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.Student','gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.Student','gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.Student',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(9, 'gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.UndergraduateStudent','gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.UndergraduateStudent','gov.nih.nci.cacoresdk.domain.inheritance.multiplechild.UndergraduateStudent',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(10, 'gov.nih.nci.cacoresdk.domain.inheritance.onechild.Human','gov.nih.nci.cacoresdk.domain.inheritance.onechild.Human','gov.nih.nci.cacoresdk.domain.inheritance.onechild.Human',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(11, 'gov.nih.nci.cacoresdk.domain.inheritance.onechild.Mammal','gov.nih.nci.cacoresdk.domain.inheritance.onechild.Mammal','gov.nih.nci.cacoresdk.domain.inheritance.onechild.Mammal',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(12, 'gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.Assistant','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.Assistant','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.Assistant',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(13, 'gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.AssistantProfessor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.AssistantProfessor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.AssistantProfessor',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(14, 'gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.AssociateProfessor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.AssociateProfessor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.AssociateProfessor',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(15, 'gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.Professor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.Professor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.Professor',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(16, 'gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.TenuredProfessor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.TenuredProfessor','gov.nih.nci.cacoresdk.domain.inheritance.parentwithassociation.TenuredProfessor',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(17, 'gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.CRTMonitor','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.CRTMonitor','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.CRTMonitor',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(18, 'gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.Display','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.Display','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.Display',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(19, 'gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.LCDMonitor','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.LCDMonitor','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.LCDMonitor',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(20, 'gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.Monitor','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.Monitor','gov.nih.nci.cacoresdk.domain.inheritance.twolevelinheritance.Monitor',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(21, 'gov.nih.nci.cacoresdk.domain.manytomany.bidirectional.Employee','gov.nih.nci.cacoresdk.domain.manytomany.bidirectional.Employee','gov.nih.nci.cacoresdk.domain.manytomany.bidirectional.Employee',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(22, 'gov.nih.nci.cacoresdk.domain.manytomany.bidirectional.Project','gov.nih.nci.cacoresdk.domain.manytomany.bidirectional.Project','gov.nih.nci.cacoresdk.domain.manytomany.bidirectional.Project',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(23, 'gov.nih.nci.cacoresdk.domain.manytomany.unidirectional.Author','gov.nih.nci.cacoresdk.domain.manytomany.unidirectional.Author','gov.nih.nci.cacoresdk.domain.manytomany.unidirectional.Author',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(24, 'gov.nih.nci.cacoresdk.domain.manytomany.unidirectional.Book','gov.nih.nci.cacoresdk.domain.manytomany.unidirectional.Book','gov.nih.nci.cacoresdk.domain.manytomany.unidirectional.Book',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(25, 'gov.nih.nci.cacoresdk.domain.manytoone.unidirectional.Chef','gov.nih.nci.cacoresdk.domain.manytoone.unidirectional.Chef','gov.nih.nci.cacoresdk.domain.manytoone.unidirectional.Chef',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(26, 'gov.nih.nci.cacoresdk.domain.manytoone.unidirectional.Restaurant','gov.nih.nci.cacoresdk.domain.manytoone.unidirectional.Restaurant','gov.nih.nci.cacoresdk.domain.manytoone.unidirectional.Restaurant',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(27, 'gov.nih.nci.cacoresdk.domain.onetomany.bidirectional.Computer','gov.nih.nci.cacoresdk.domain.onetomany.bidirectional.Computer','gov.nih.nci.cacoresdk.domain.onetomany.bidirectional.Computer',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(28, 'gov.nih.nci.cacoresdk.domain.onetomany.bidirectional.HardDrive','gov.nih.nci.cacoresdk.domain.onetomany.bidirectional.HardDrive','gov.nih.nci.cacoresdk.domain.onetomany.bidirectional.HardDrive',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(29, 'gov.nih.nci.cacoresdk.domain.onetomany.unidirectional.Key','gov.nih.nci.cacoresdk.domain.onetomany.unidirectional.Key','gov.nih.nci.cacoresdk.domain.onetomany.unidirectional.Key',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(30, 'gov.nih.nci.cacoresdk.domain.onetomany.unidirectional.KeyChain','gov.nih.nci.cacoresdk.domain.onetomany.unidirectional.KeyChain','gov.nih.nci.cacoresdk.domain.onetomany.unidirectional.KeyChain',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(31, 'gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.OrderLine','gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.OrderLine','gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.OrderLine',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(32, 'gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.Product','gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.Product','gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.Product',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(33, 'gov.nih.nci.cacoresdk.domain.onetoone.multipleassociation.Child','gov.nih.nci.cacoresdk.domain.onetoone.multipleassociation.Child','gov.nih.nci.cacoresdk.domain.onetoone.multipleassociation.Child',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(34, 'gov.nih.nci.cacoresdk.domain.onetoone.multipleassociation.Parent','gov.nih.nci.cacoresdk.domain.onetoone.multipleassociation.Parent','gov.nih.nci.cacoresdk.domain.onetoone.multipleassociation.Parent',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(35, 'gov.nih.nci.cacoresdk.domain.onetoone.unidirectional.Address','gov.nih.nci.cacoresdk.domain.onetoone.unidirectional.Address','gov.nih.nci.cacoresdk.domain.onetoone.unidirectional.Address',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(36, 'gov.nih.nci.cacoresdk.domain.onetoone.unidirectional.Person','gov.nih.nci.cacoresdk.domain.onetoone.unidirectional.Person','gov.nih.nci.cacoresdk.domain.onetoone.unidirectional.Person',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(37, 'gov.nih.nci.cacoresdk.domain.other.datatype.AllDataType','gov.nih.nci.cacoresdk.domain.other.datatype.AllDataType','gov.nih.nci.cacoresdk.domain.other.datatype.AllDataType',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(38, 'gov.nih.nci.cacoresdk.domain.other.levelassociation.Card','gov.nih.nci.cacoresdk.domain.other.levelassociation.Card','gov.nih.nci.cacoresdk.domain.other.levelassociation.Card',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(39, 'gov.nih.nci.cacoresdk.domain.other.levelassociation.Deck','gov.nih.nci.cacoresdk.domain.other.levelassociation.Deck','gov.nih.nci.cacoresdk.domain.other.levelassociation.Deck',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(40, 'gov.nih.nci.cacoresdk.domain.other.levelassociation.Hand','gov.nih.nci.cacoresdk.domain.other.levelassociation.Hand','gov.nih.nci.cacoresdk.domain.other.levelassociation.Hand',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(41, 'gov.nih.nci.cacoresdk.domain.other.levelassociation.Suit','gov.nih.nci.cacoresdk.domain.other.levelassociation.Suit','gov.nih.nci.cacoresdk.domain.other.levelassociation.Suit',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(42, 'gov.nih.nci.cacoresdk.domain.other.primarykey.DoubleKey','gov.nih.nci.cacoresdk.domain.other.primarykey.DoubleKey','gov.nih.nci.cacoresdk.domain.other.primarykey.DoubleKey',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(43, 'gov.nih.nci.cacoresdk.domain.other.primarykey.FloatKey','gov.nih.nci.cacoresdk.domain.other.primarykey.FloatKey','gov.nih.nci.cacoresdk.domain.other.primarykey.FloatKey',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(44, 'gov.nih.nci.cacoresdk.domain.other.primarykey.IntegerKey','gov.nih.nci.cacoresdk.domain.other.primarykey.IntegerKey','gov.nih.nci.cacoresdk.domain.other.primarykey.IntegerKey',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
+
+insert into csm_protection_element(PROTECTION_ELEMENT_ID, PROTECTION_ELEMENT_NAME,PROTECTION_ELEMENT_DESCRIPTION,OBJECT_ID,APPLICATION_ID,UPDATE_DATE)
+values(45, 'gov.nih.nci.cacoresdk.domain.other.primarykey.StringKey','gov.nih.nci.cacoresdk.domain.other.primarykey.StringKey','gov.nih.nci.cacoresdk.domain.other.primarykey.StringKey',2,sysdate);
+select CSM_PROTECTIO_PROTECTION_E_SEQ.nextval from dual;
+
+COMMIT;
+
