52b074e7085a32b693f424dd8907b9a46b4d3713
Satish Patel <satish79>
2007-05-22 19:37:52 +0000

    Added constructors and toString
    SVN-Revision: 679

--- SDK4/codegen/core/src/gov/nih/nci/codegen/GeneratorError.java
+++ SDK4/codegen/core/src/gov/nih/nci/codegen/GeneratorError.java
@@ -2,5 +2,21 @@ package gov.nih.nci.codegen;
 
 public class GeneratorError
 {
+	private String message;
+	private GenerationException ge;
 	
+	public GeneratorError(String message, GenerationException ge) {
+		this.message = message;
+		this.ge = ge;
+	}
+	
+	public GeneratorError(String message)
+	{
+		this.message = message;
+	}
+	
+	public String toString()
+	{
+		return message + ge.toString();
+	}
 }
