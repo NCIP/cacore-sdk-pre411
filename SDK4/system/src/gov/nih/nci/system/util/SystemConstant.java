27776b8a9bf3f7341ffb32b38b744d104f972130
Satish Patel <satish79>
2007-07-11 18:34:58 +0000

    Removed the setters for the unwanted properties and removed unwanted constants
    SVN-Revision: 813

--- SDK4/system/src/gov/nih/nci/system/util/SystemConstant.java
+++ SDK4/system/src/gov/nih/nci/system/util/SystemConstant.java
@@ -23,17 +23,4 @@ public class SystemConstant {
 	public static final String  BACK_SLASH="\\";		
 	public static final String 	COMMA_STR=",";
 	public static final String 	FORWARD_SLASH_STR="/";
-	
-	public int MAX_RESULT_COUNT_PER_QUERY = 5000;
-	public int RESULT_COUNT_PER_QUERY = 1000;	
-	
-    public static final String 	XLINK_URL = "http://www.w3.org/1999/xlink";
-
-	public void setMAX_RESULT_COUNT_PER_QUERY(int max_result_count_per_query) {
-		MAX_RESULT_COUNT_PER_QUERY = max_result_count_per_query;
-	}
-
-	public void setRESULT_COUNT_PER_QUERY(int result_count_per_query) {
-		RESULT_COUNT_PER_QUERY = result_count_per_query;
-	}	
 }
