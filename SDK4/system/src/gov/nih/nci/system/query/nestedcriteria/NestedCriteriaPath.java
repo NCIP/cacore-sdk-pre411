e0e7cc6167becc6804980fb97776e9810c9ffa61
Satish Patel <satish79>
2007-07-16 15:14:42 +0000

    Wrapper class to support nested criteria as supplied by the user
    SVN-Revision: 854

---
+++ SDK4/system/src/gov/nih/nci/system/query/nestedcriteria/NestedCriteriaPath.java
@@ -0,0 +1,39 @@
+package gov.nih.nci.system.query.nestedcriteria;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Satish Patel
+ *
+ */
+public class NestedCriteriaPath implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String pathString;
+	private List parameters;	
+	
+	public NestedCriteriaPath(String pathString)
+	{
+		this.pathString = pathString;
+	}
+	
+	public NestedCriteriaPath(String pathString, List parameters)
+	{
+		this.pathString = pathString;
+		this.parameters = parameters;
+	}	
+
+	public String getpathString()
+	{
+		return this.pathString;
+	}
+
+	public List getParameters() {
+		return parameters;
+	}
+
+}
