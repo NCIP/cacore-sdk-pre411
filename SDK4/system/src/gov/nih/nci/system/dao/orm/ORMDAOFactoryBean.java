314b51ad657f02c2815df4904b5953e8a9d84fca
Kunal Modi <modik@mail.nih.gov>
2007-09-21 16:40:14 +0000

    Added the checks to enable Instance Level Security and add the filters and also to enable Attribute Level Security
    SVN-Revision: 1154

--- SDK4/system/src/gov/nih/nci/system/dao/orm/ORMDAOFactoryBean.java
+++ SDK4/system/src/gov/nih/nci/system/dao/orm/ORMDAOFactoryBean.java
@@ -1,5 +1,10 @@
 package gov.nih.nci.system.dao.orm;
 
+import gov.nih.nci.security.AuthorizationManager;
+import gov.nih.nci.security.acegi.CSMAfterInvocationProvider;
+import gov.nih.nci.security.acegi.authentication.CSMAuthenticationProvider;
+import gov.nih.nci.security.authorization.instancelevel.InstanceLevelSecurityHelper;
+
 import java.util.Map;
 import java.util.Properties;
 
@@ -11,44 +16,44 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
 
-public class ORMDAOFactoryBean implements FactoryBean {
+public class ORMDAOFactoryBean extends LocalSessionFactoryBean {
 	
 	private static Logger log = Logger.getLogger(ORMDAOFactoryBean.class.getName());
 	
-	private LocalSessionFactoryBean lsfb;
-	
 	private ORMDAOImpl ormDAO;
-
+	
+	private AuthorizationManager authorizationManager = null;
+	private CSMAuthenticationProvider authenticationProvider = null;
+	
+	boolean securityEnabledFlag = false;
+	boolean instanceLevelSecurity = false;
+	boolean attributeLevelSecurity = false;
+	boolean caseSensitive;
+	int resultCountPerQuery;
+	
 	public ORMDAOFactoryBean(String configLocation, Properties systemProperties, Map systemPropertiesMap) throws Exception {	
-		lsfb = new LocalSessionFactoryBean();
+	
 		Resource resource = new ClassPathResource(configLocation);
-		lsfb.setConfigLocation(resource);
+		this.setConfigLocation(resource);
 		
-		boolean caseSensitive;
-		int resultCountPerQuery;
-		
-		boolean instanceLevelSecurity;
-		boolean attributeLevelSecurity;
 		try {
-			caseSensitive = Boolean.parseBoolean(systemProperties.getProperty("caseSensitive"));
-			resultCountPerQuery = Integer.parseInt(systemProperties.getProperty("resultCountPerQuery"));
-			
-			String instanceLevelSecurityEnabled = (String)systemProperties.getProperty("instanceLevelSecurityEnabled");
-			String attributeLevelSecurityEnabled = (String)systemProperties.getProperty("attributeLevelSecurityEnabled");
-			instanceLevelSecurity = "yes".equalsIgnoreCase(instanceLevelSecurityEnabled) || "true".equalsIgnoreCase(instanceLevelSecurityEnabled);
-			attributeLevelSecurity = "yes".equalsIgnoreCase(attributeLevelSecurityEnabled) || "true".equalsIgnoreCase(attributeLevelSecurityEnabled);
-			
-			//TODO CSMTEAM
-			//If InstanceLevel and or attribute level security is enabled then set corresponding filters
-			//lsfb.setFilterDefinitions(org.hibernate.engine.FilterDefinition[]);
-			
-			lsfb.afterPropertiesSet();
+			this.caseSensitive = Boolean.parseBoolean(systemProperties.getProperty("caseSensitive"));
+			this.resultCountPerQuery = Integer.parseInt(systemProperties.getProperty("resultCountPerQuery"));
+			String securityEnabled = (String)systemProperties.getProperty("securityEnabled");
+			if ("yes".equalsIgnoreCase(securityEnabled) || "true".equalsIgnoreCase(securityEnabled))
+			{
+				securityEnabledFlag = true;
+				String instanceLevelSecurityEnabled = (String)systemProperties.getProperty("instanceLevelSecurityEnabled");
+				String attributeLevelSecurityEnabled = (String)systemProperties.getProperty("attributeLevelSecurityEnabled");
+				instanceLevelSecurity = "yes".equalsIgnoreCase(instanceLevelSecurityEnabled) || "true".equalsIgnoreCase(instanceLevelSecurityEnabled);
+				attributeLevelSecurity = "yes".equalsIgnoreCase(attributeLevelSecurityEnabled) || "true".equalsIgnoreCase(attributeLevelSecurityEnabled);
+				this.authenticationProvider = (CSMAuthenticationProvider)systemPropertiesMap.get("AUTHENTICATION_PROVIDER");
+				this.authorizationManager = authenticationProvider.getUserDetailsService().authorizationManagerInstance();
+			}		
 		} catch (Exception e){
 			log.error(e);
 			throw e;
 		}
-		
-		ormDAO = new ORMDAOImpl((SessionFactory)lsfb.getObject(), (Configuration)lsfb.getConfiguration(), caseSensitive, resultCountPerQuery, instanceLevelSecurity, attributeLevelSecurity);
 	}
 	
 	public Object getObject() {
@@ -61,5 +66,19 @@ public class ORMDAOFactoryBean implements FactoryBean {
 	
 	public boolean isSingleton(){
 		return true;
+	}
+	
+	public void afterPropertiesSet() throws Exception
+	{
+		super.afterPropertiesSet();
+		if (securityEnabledFlag)
+			ormDAO = new ORMDAOImpl((SessionFactory)this.getSessionFactory(), (Configuration)this.getConfiguration(), caseSensitive, resultCountPerQuery, instanceLevelSecurity, attributeLevelSecurity, authenticationProvider);
+		else
+			ormDAO = new ORMDAOImpl((SessionFactory)this.getSessionFactory(), (Configuration)this.getConfiguration(), caseSensitive, resultCountPerQuery, instanceLevelSecurity, attributeLevelSecurity, null);			
+	}
+	protected void postProcessConfiguration(Configuration configuration)
+	{
+		if (instanceLevelSecurity)
+			InstanceLevelSecurityHelper.addFilters(authorizationManager, configuration);
 	}
 }
