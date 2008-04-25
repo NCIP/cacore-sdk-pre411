b9a9e8212655377d6b20b2a445b438fcffeedd77
Satish Patel <satish79>
2008-04-25 19:32:09 +0000

    created getter and setter methods for private variables. This is to ensure that the factory can be extended
    SVN-Revision: 1864

--- SDK4/system/src/gov/nih/nci/system/dao/orm/ORMDAOFactoryBean.java
+++ SDK4/system/src/gov/nih/nci/system/dao/orm/ORMDAOFactoryBean.java
@@ -1,7 +1,6 @@
 package gov.nih.nci.system.dao.orm;
 
 import gov.nih.nci.security.AuthorizationManager;
-import gov.nih.nci.security.acegi.CSMAfterInvocationProvider;
 import gov.nih.nci.security.acegi.authentication.CSMAuthenticationProvider;
 import gov.nih.nci.security.authorization.instancelevel.InstanceLevelSecurityHelper;
 
@@ -11,7 +10,6 @@ import java.util.Properties;
 import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
@@ -25,11 +23,11 @@ public class ORMDAOFactoryBean extends LocalSessionFactoryBean {
 	private AuthorizationManager authorizationManager = null;
 	private CSMAuthenticationProvider authenticationProvider = null;
 	
-	boolean securityEnabledFlag = false;
-	boolean instanceLevelSecurity = false;
-	boolean attributeLevelSecurity = false;
-	boolean caseSensitive;
-	int resultCountPerQuery;
+	private boolean securityEnabledFlag = false;
+	private boolean instanceLevelSecurity = false;
+	private boolean attributeLevelSecurity = false;
+	private boolean caseSensitive;
+	private int resultCountPerQuery;
 	
 	public ORMDAOFactoryBean(String configLocation, Properties systemProperties, Map systemPropertiesMap) throws Exception {	
 	
@@ -71,14 +69,85 @@ public class ORMDAOFactoryBean extends LocalSessionFactoryBean {
 	public void afterPropertiesSet() throws Exception
 	{
 		super.afterPropertiesSet();
-		if (securityEnabledFlag)
-			ormDAO = new ORMDAOImpl((SessionFactory)this.getSessionFactory(), (Configuration)this.getConfiguration(), caseSensitive, resultCountPerQuery, instanceLevelSecurity, attributeLevelSecurity, authenticationProvider);
-		else
-			ormDAO = new ORMDAOImpl((SessionFactory)this.getSessionFactory(), (Configuration)this.getConfiguration(), caseSensitive, resultCountPerQuery, instanceLevelSecurity, attributeLevelSecurity, null);			
+		createDAO();
 	}
+	
+	protected void createDAO()
+	{
+		if (isSecurityEnabledFlag())
+			setOrmDAO(new ORMDAOImpl((SessionFactory)this.getSessionFactory(), (Configuration)this.getConfiguration(), isCaseSensitive(), getResultCountPerQuery(), isInstanceLevelSecurity(), isAttributeLevelSecurity(), getAuthenticationProvider()));
+		else
+			setOrmDAO(new ORMDAOImpl((SessionFactory)this.getSessionFactory(), (Configuration)this.getConfiguration(), isCaseSensitive(), getResultCountPerQuery(), isInstanceLevelSecurity(), isAttributeLevelSecurity(), null));
+	}
+	
 	protected void postProcessConfiguration(Configuration configuration)
 	{
 		if (instanceLevelSecurity)
 			InstanceLevelSecurityHelper.addFilters(authorizationManager, configuration);
+	}
+
+	protected boolean isAttributeLevelSecurity() {
+		return attributeLevelSecurity;
+	}
+
+	protected void setAttributeLevelSecurity(boolean attributeLevelSecurity) {
+		this.attributeLevelSecurity = attributeLevelSecurity;
+	}
+
+	protected CSMAuthenticationProvider getAuthenticationProvider() {
+		return authenticationProvider;
+	}
+
+	protected void setAuthenticationProvider(
+			CSMAuthenticationProvider authenticationProvider) {
+		this.authenticationProvider = authenticationProvider;
+	}
+
+	protected AuthorizationManager getAuthorizationManager() {
+		return authorizationManager;
+	}
+
+	protected void setAuthorizationManager(AuthorizationManager authorizationManager) {
+		this.authorizationManager = authorizationManager;
+	}
+
+	protected boolean isCaseSensitive() {
+		return caseSensitive;
+	}
+
+	protected void setCaseSensitive(boolean caseSensitive) {
+		this.caseSensitive = caseSensitive;
+	}
+
+	protected boolean isInstanceLevelSecurity() {
+		return instanceLevelSecurity;
+	}
+
+	protected void setInstanceLevelSecurity(boolean instanceLevelSecurity) {
+		this.instanceLevelSecurity = instanceLevelSecurity;
+	}
+
+	protected ORMDAOImpl getOrmDAO() {
+		return ormDAO;
+	}
+
+	protected void setOrmDAO(ORMDAOImpl ormDAO) {
+		this.ormDAO = ormDAO;
+	}
+
+	protected int getResultCountPerQuery() {
+		return resultCountPerQuery;
+	}
+
+	protected void setResultCountPerQuery(int resultCountPerQuery) {
+		this.resultCountPerQuery = resultCountPerQuery;
+	}
+
+	protected boolean isSecurityEnabledFlag() {
+		return securityEnabledFlag;
+	}
+
+	protected void setSecurityEnabledFlag(boolean securityEnabledFlag) {
+		this.securityEnabledFlag = securityEnabledFlag;
 	}
 }
