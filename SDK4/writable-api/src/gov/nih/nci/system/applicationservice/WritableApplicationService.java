b1ef60b98fec86783053cdc3e82d21afd522fc49
Satish Patel <satish79>
2008-05-07 20:39:41 +0000

    Added query operation
    SVN-Revision: 1902

--- SDK4/writable-api/src/gov/nih/nci/system/applicationservice/WritableApplicationService.java
+++ SDK4/writable-api/src/gov/nih/nci/system/applicationservice/WritableApplicationService.java
@@ -1,8 +1,9 @@
 package gov.nih.nci.system.applicationservice;
 
 import gov.nih.nci.system.applicationservice.ApplicationService;
+import gov.nih.nci.system.query.SDKQuery;
 
 public interface WritableApplicationService extends ApplicationService
 {
-	
+	public Object executeQuery(SDKQuery query) throws ApplicationException;
 }
--- SDK4/writable-api/src/gov/nih/nci/system/applicationservice/impl/WritableApplicationServiceImpl.java
+++ SDK4/writable-api/src/gov/nih/nci/system/applicationservice/impl/WritableApplicationServiceImpl.java
@@ -1,15 +1,54 @@
 package gov.nih.nci.system.applicationservice.impl;
 
+import gov.nih.nci.system.applicationservice.ApplicationException;
 import gov.nih.nci.system.applicationservice.WritableApplicationService;
+import gov.nih.nci.system.dao.DAO;
+import gov.nih.nci.system.dao.DAOException;
+import gov.nih.nci.system.dao.Request;
+import gov.nih.nci.system.dao.Response;
+import gov.nih.nci.system.dao.WritableDAO;
+import gov.nih.nci.system.query.SDKQuery;
+import gov.nih.nci.system.query.example.ExampleQuery;
 import gov.nih.nci.system.util.ClassCache;
 
-import java.util.Properties;
+import org.apache.log4j.Logger;
 
 
 public class WritableApplicationServiceImpl extends ApplicationServiceImpl implements WritableApplicationService
 {
-	public WritableApplicationServiceImpl(ClassCache classCache, Properties systemProperties) {
-		super(classCache, systemProperties);
+	private static Logger log = Logger.getLogger(WritableApplicationServiceImpl.class.getName());
+	public WritableApplicationServiceImpl(ClassCache classCache) {
+		super(classCache);
+	}
+
+	public Object executeQuery(SDKQuery query) throws ApplicationException {
+		String classname = null;
+		if(query instanceof ExampleQuery)
+			classname = ((ExampleQuery)query).getExample().getClass().getName();
+		WritableDAO dao = getWritableDAO(classname);
+		Request request = new Request();
+		request.setIsCount(Boolean.FALSE);
+		request.setClassCache(getClassCache());
+		request.setRequest(query);
+		try {
+			Response resp =  dao.query(request);
+			return resp.getResponse();
+		}  catch(DAOException daoException) {
+			log.error("Error while querying DAO ",daoException);
+			throw daoException;
+		} catch(Exception exception) {
+			log.error("Error while querying DAO ", exception);
+			throw new ApplicationException("Error while querying DAO: ", exception);
+		}
+	}
+	
+	protected WritableDAO getWritableDAO(String classname) throws ApplicationException
+	{
+		DAO dao = getDAO(classname);
+		if(dao instanceof WritableDAO)
+			return (WritableDAO)dao;
+
+		throw new ApplicationException("Can not execute query on non-writable DAO");
 	}
 
 }
