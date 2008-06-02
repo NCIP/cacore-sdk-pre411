b555889c7130306de5f76d07c56ba3cf032fe58e
Satish Patel <satish79>
2008-06-02 17:41:07 +0000

    Changed the return type of the method
    SVN-Revision: 1981

--- SDK4/writable-api/src/gov/nih/nci/system/applicationservice/WritableApplicationService.java
+++ SDK4/writable-api/src/gov/nih/nci/system/applicationservice/WritableApplicationService.java
@@ -2,8 +2,9 @@ package gov.nih.nci.system.applicationservice;
 
 import gov.nih.nci.system.applicationservice.ApplicationService;
 import gov.nih.nci.system.query.SDKQuery;
+import gov.nih.nci.system.query.SDKQueryResult;
 
 public interface WritableApplicationService extends ApplicationService
 {
-	public Object executeQuery(SDKQuery query) throws ApplicationException;
+	public SDKQueryResult executeQuery(SDKQuery query) throws ApplicationException;
 }
