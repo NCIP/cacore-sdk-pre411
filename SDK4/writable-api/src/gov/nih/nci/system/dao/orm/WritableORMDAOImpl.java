f295fa5a2dd64cbde15d86b0287c8397f966ad1d
Satish Patel <satish79>
2008-06-02 17:42:01 +0000

    completed the operations for all writable api
    SVN-Revision: 1983

--- SDK4/writable-api/src/gov/nih/nci/system/dao/orm/WritableORMDAOImpl.java
+++ SDK4/writable-api/src/gov/nih/nci/system/dao/orm/WritableORMDAOImpl.java
@@ -5,66 +5,127 @@ import gov.nih.nci.system.dao.Request;
 import gov.nih.nci.system.dao.Response;
 import gov.nih.nci.system.dao.WritableDAO;
 import gov.nih.nci.system.query.SDKQuery;
+import gov.nih.nci.system.query.SDKQueryResult;
 import gov.nih.nci.system.query.example.DeleteExampleQuery;
 import gov.nih.nci.system.query.example.InsertExampleQuery;
-import gov.nih.nci.system.query.example.SearchExampleQuery;
 import gov.nih.nci.system.query.example.UpdateExampleQuery;
+import gov.nih.nci.system.query.hql.DeleteHQLQuery;
+import gov.nih.nci.system.query.hql.InsertHQLQuery;
+import gov.nih.nci.system.query.hql.UpdateHQLQuery;
 
+import java.sql.SQLException;
 import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
 
 public class WritableORMDAOImpl extends ORMDAOImpl implements WritableDAO
 {
-
 	@Override
 	public Response query(Request request) throws DAOException 
 	{
 		if(! (request.getRequest() instanceof SDKQuery))
 			return super.query(request);
 
-		Object retObj = null;
+		SDKQueryResult result = null;
+		
 		try 
 		{
 			SDKQuery q = (SDKQuery) request.getRequest();
-			if (q instanceof SearchExampleQuery)
-				retObj = search(((SearchExampleQuery) q).getExample());
-			else if (q instanceof InsertExampleQuery)
-				retObj = insert(((InsertExampleQuery) q).getExample());
+			if (q instanceof InsertExampleQuery)
+			{
+				Object retObj = insert(((InsertExampleQuery) q).getExample());
+				result = new SDKQueryResult(retObj);
+			}
 			else if (q instanceof DeleteExampleQuery)
+			{
 				delete(((DeleteExampleQuery) q).getExample());
-			else if (q instanceof UpdateExampleQuery) {
+				result = new SDKQueryResult(true);
+			}
+			else if (q instanceof UpdateExampleQuery) 
+			{
 				update(((UpdateExampleQuery) q).getExample());
-				retObj = ((UpdateExampleQuery) q).getExample();
+				result = new SDKQueryResult(((UpdateExampleQuery) q).getExample());
+			}
+			else if(q instanceof InsertHQLQuery)
+			{
+				insert(((InsertHQLQuery)q).getHqlString(),((InsertHQLQuery)q).getParameters());
+				result = new SDKQueryResult(true);
+			}
+			else if(q instanceof DeleteHQLQuery)
+			{
+				update(((DeleteHQLQuery)q).getHqlString(),((DeleteHQLQuery)q).getParameters());
+				result = new SDKQueryResult(true);
+			}
+			else if(q instanceof UpdateHQLQuery)
+			{
+				delete(((UpdateHQLQuery)q).getHqlString(),((UpdateHQLQuery)q).getParameters());
+				result = new SDKQueryResult(true);
 			}
 		} catch (Exception e) {
 			throw new DAOException(e);
 		} 
 
 		Response resp = new Response();
-		resp.setResponse(retObj);
+		resp.setResponse(result);
 		return resp;
 	}
 
-	protected Object insert(Object o) throws Exception
+	public Object insert(Object o) throws Exception
 	{
 		log.info("In the writable DAO. executing the Insert query");
 		return getHibernateTemplate().save(o);
 	}
 
-	protected void update(Object o) throws Exception
+	public void update(Object o) throws Exception
 	{
 		log.info("In the writable DAO. executing the Update query");
 		getHibernateTemplate().update(o);
 	}
 
-	protected void delete(Object o) throws Exception
+	public void delete(Object o) throws Exception
 	{
 		log.info("In the writable DAO. executing the Delete query");
 		getHibernateTemplate().delete(o);
 	}
 
-	protected List search(Object o) throws Exception
+	public void insert(final String hql, final List<Object> paramList) 
 	{
-		log.info("In the writable DAO. executing the Search query");
-		return getHibernateTemplate().findByExample(o);
+		log.info("In the writable DAO. executing the Insert query");
+		HibernateCallback callBack = getExecuteUpdateHibernateCallback(hql,paramList);
+		getHibernateTemplate().execute(callBack);
+	}
+
+	public void update(String hql, List<Object> paramList)
+	{
+		log.info("In the writable DAO. executing the Update query");
+		HibernateCallback callBack = getExecuteUpdateHibernateCallback(hql,paramList);
+		getHibernateTemplate().execute(callBack);
+	}
+
+	public void delete(String hql, List<Object> paramList)
+	{
+		log.info("In the writable DAO. executing the Delete query");
+		HibernateCallback callBack = getExecuteUpdateHibernateCallback(hql,paramList);
+		getHibernateTemplate().execute(callBack);
+	}
+	
+	protected HibernateCallback getExecuteUpdateHibernateCallback(final String hql, final List<Object> paramList)
+	{
+		HibernateCallback callBack = new HibernateCallback(){
+
+			public Object doInHibernate(Session session) throws HibernateException, SQLException {
+				Query q = session.createQuery(hql);
+				int count = 0;
+				if(paramList!=null)
+					for(Object obj:paramList)
+						q.setParameter(count++,obj);
+				int result = q.executeUpdate();
+				return result;
+			}
+		};
+		return callBack;
 	}
 }
