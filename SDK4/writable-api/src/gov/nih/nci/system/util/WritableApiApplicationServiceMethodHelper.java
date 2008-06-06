b5c9cb4f5a95b19a099a004bcbc97f9a46eb0fb8
Santosh garmilla <garmillas>
2008-06-06 14:58:50 +0000

    changed method return type for method getDomainObjectName from Map<String,Collection> to Map<String,String>
    SVN-Revision: 2026

--- SDK4/writable-api/src/gov/nih/nci/system/util/WritableApiApplicationServiceMethodHelper.java
+++ SDK4/writable-api/src/gov/nih/nci/system/util/WritableApiApplicationServiceMethodHelper.java
@@ -4,9 +4,9 @@ import gov.nih.nci.system.query.SDKQuery;
 import gov.nih.nci.system.query.example.DeleteExampleQuery;
 import gov.nih.nci.system.query.example.InsertExampleQuery;
 import gov.nih.nci.system.query.example.UpdateExampleQuery;
+import gov.nih.nci.system.security.SecurityConstants;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,35 +16,35 @@ import org.aopalliance.intercept.MethodInvocation;
 public class WritableApiApplicationServiceMethodHelper extends ApplicationServiceMethodHelper{
 	
 	@SuppressWarnings("unchecked")
-	public Map<String, Collection<String>> getDomainObjectName(MethodInvocation invocation) {
-		Collection<String> privileges = new ArrayList<String>();
+	public Map<String,String> getDomainObjectName(MethodInvocation invocation) {
+		String privilege = null;
 		
 		String domainObjectName = "*";
 		Method method = invocation.getMethod();
 		Object[] arguments = invocation.getArguments();
 		Map domainObjectMap=new HashMap<String, Collection<String>>();
-		Map<String, Collection<String>> securityMap=new HashMap<String, Collection<String>>();
+		Map<String, String> securityMap = new HashMap<String, String>();
 
 		if ("executeQuery".equals(method.getName()) && (arguments[0] instanceof SDKQuery)) {
 			SDKQuery sdkQuery = (SDKQuery) arguments[0];
 
 			if (sdkQuery instanceof InsertExampleQuery) {
-				privileges.add(SecurityConstants.CREATE);
+				privilege=SecurityConstants.CREATE;
 				InsertExampleQuery insertExampleQuery = (InsertExampleQuery) sdkQuery;
-				domainObjectMap.put(insertExampleQuery.getExample().getClass().getName(), privileges);
+				domainObjectMap.put(insertExampleQuery.getExample().getClass().getName(), privilege);
 				return domainObjectMap;
 			} else if (sdkQuery instanceof DeleteExampleQuery) {
-				privileges.add(SecurityConstants.DELETE);
+				privilege=SecurityConstants.DELETE;
 				DeleteExampleQuery deleteExampleQuery = (DeleteExampleQuery) sdkQuery;
-				domainObjectMap.put(deleteExampleQuery.getExample().getClass().getName(), privileges);
+				domainObjectMap.put(deleteExampleQuery.getExample().getClass().getName(), privilege);
 				return domainObjectMap;
 			} else if (sdkQuery instanceof UpdateExampleQuery) {
-				privileges.add(SecurityConstants.UPDATE);
+				privilege=SecurityConstants.UPDATE;
 				UpdateExampleQuery updateExampleQuery = (UpdateExampleQuery) sdkQuery;
-				domainObjectMap.put(updateExampleQuery.getExample().getClass().getName(), privileges);
+				domainObjectMap.put(updateExampleQuery.getExample().getClass().getName(), privilege);
 				return domainObjectMap;
 			}
-			securityMap.put(domainObjectName, privileges);
+			securityMap.put(domainObjectName, privilege);
 		} else {
 			securityMap = super.getDomainObjectName(invocation);
 		}
