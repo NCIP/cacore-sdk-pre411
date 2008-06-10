665c6709da0afaadf06fe8d093fc6c817c5686aa
Santosh garmilla <garmillas>
2008-06-10 17:57:44 +0000

    added class implements SDKQuery
    SVN-Revision: 2044

--- SDK4/writable-api/src/gov/nih/nci/system/query/hql/SearchHQLQuery.java
+++ SDK4/writable-api/src/gov/nih/nci/system/query/hql/SearchHQLQuery.java
@@ -1,10 +1,11 @@
 package gov.nih.nci.system.query.hql;
 
+import gov.nih.nci.system.query.SDKQuery;
 import gov.nih.nci.system.query.hibernate.HQLCriteria;
 
 import java.util.List;
 
-public class SearchHQLQuery extends HQLCriteria implements HQLSearchQuery
+public class SearchHQLQuery extends HQLCriteria implements HQLSearchQuery,SDKQuery
 {
 	private static final long serialVersionUID = 1L;
 
--- SDK4/writable-api/src/gov/nih/nci/system/query/hql/UpdateHQLQuery.java
+++ SDK4/writable-api/src/gov/nih/nci/system/query/hql/UpdateHQLQuery.java
@@ -2,9 +2,10 @@ package gov.nih.nci.system.query.hql;
 
 import java.util.List;
 
+import gov.nih.nci.system.query.SDKQuery;
 import gov.nih.nci.system.query.hibernate.HQLCriteria;
 
-public class UpdateHQLQuery extends HQLCriteria implements HQLManipulationQuery
+public class UpdateHQLQuery extends HQLCriteria implements HQLManipulationQuery,SDKQuery
 {
 	private static final long serialVersionUID = 1L;
 
