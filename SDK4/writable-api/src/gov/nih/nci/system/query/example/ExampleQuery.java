a53df0a7992e337477301bba8e765eba03382f3f
Santosh garmilla <garmillas>
2008-06-14 06:00:18 +0000

    added setExample()
    SVN-Revision: 2071

--- SDK4/writable-api/src/gov/nih/nci/system/query/example/ExampleQuery.java
+++ SDK4/writable-api/src/gov/nih/nci/system/query/example/ExampleQuery.java
@@ -2,16 +2,18 @@ package gov.nih.nci.system.query.example;
 
 import gov.nih.nci.system.query.SDKQuery;
 
-public abstract class ExampleQuery implements SDKQuery
-{
+public abstract class ExampleQuery implements SDKQuery {
 	private Object example;
-	
-	public ExampleQuery(Object example)
-	{
+
+	public ExampleQuery(Object example) {
 		this.example = example;
 	}
 
 	public Object getExample() {
 		return example;
+	}
+
+	public void setExample(Object example) {
+		this.example = example;
 	}
 }
