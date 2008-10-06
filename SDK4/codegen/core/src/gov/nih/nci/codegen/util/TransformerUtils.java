13f7b62d59cb55afbc6434c6dbcf4e9c0c3f741b
Daniel Dumitru <ddumitru>
2008-10-06 16:01:56 +0000

    Removed extraneous "import" statement
    SVN-Revision: 2353

--- SDK4/codegen/core/src/gov/nih/nci/codegen/util/TransformerUtils.java
+++ SDK4/codegen/core/src/gov/nih/nci/codegen/util/TransformerUtils.java
@@ -30,7 +30,6 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.Vector;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
