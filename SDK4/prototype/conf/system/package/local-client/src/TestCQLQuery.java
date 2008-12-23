afed7df98504596bac68557ee0cdc9d1418a97fc
Daniel Dumitru <ddumitru>
2008-12-23 23:28:45 +0000

    Backup various TestClient version not included with standard SDK distribution
    SVN-Revision: 2514

---
+++ SDK4/prototype/conf/system/package/local-client/src/TestCQLQuery.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Property;
+
+import gov.nih.nci.cacoresdk.domain.other.levelassociation.Suit;
+import gov.nih.nci.system.client.ApplicationServiceProvider;
+import gov.nih.nci.system.query.cql.CQLAttribute;
+import gov.nih.nci.system.query.cql.CQLObject;
+import gov.nih.nci.system.query.cql.CQLPredicate;
+import gov.nih.nci.system.query.cql.CQLQuery;
+import gov.nih.nci.system.query.hibernate.HQLCriteria;
+import gov.nih.nci.system.applicationservice.ApplicationService;
+
+
+public class TestCQLQuery extends TestClient
+{
+	public static void main(String args[])
+	{
+		TestCQLQuery client = new TestCQLQuery();
+		try
+		{
+			client.testSearch();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void testSearch() throws Exception
+	{
+		ApplicationService appService = ApplicationServiceProvider.getApplicationService();
+
+		CQLQuery cqlQuery = new CQLQuery();
+		
+		CQLObject target = new CQLObject();
+		target.setName("gov.nih.nci.cacoresdk.domain.other.levelassociation.Suit");
+		
+		CQLAttribute attribute = new CQLAttribute();
+		attribute.setName("id");
+		attribute.setValue("3");  // Suit = Diamond
+		attribute.setPredicate(CQLPredicate.EQUAL_TO);
+		
+		target.setAttribute(attribute);
+		
+		cqlQuery.setTarget(target);
+
+		Collection results = appService.query(cqlQuery);
+		System.out.println("Number of qualifying records: " + results.size());
+		for(Object obj : results)
+		{
+			printObject(obj, Suit.class);
+			break;
+		}
+	}
+}
---
+++ SDK4/prototype/conf/system/package/remote-client/src/TestNestedSearchQuery.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Property;
+
+import gov.nih.nci.cacoresdk.domain.other.levelassociation.Card;
+import gov.nih.nci.cacoresdk.domain.other.levelassociation.Deck;
+import gov.nih.nci.cacoresdk.domain.other.levelassociation.Suit;
+import gov.nih.nci.system.client.ApplicationServiceProvider;
+import gov.nih.nci.system.query.cql.CQLAssociation;
+import gov.nih.nci.system.query.cql.CQLAttribute;
+import gov.nih.nci.system.query.cql.CQLObject;
+import gov.nih.nci.system.query.cql.CQLPredicate;
+import gov.nih.nci.system.query.cql.CQLQuery;
+import gov.nih.nci.system.query.hibernate.HQLCriteria;
+import gov.nih.nci.system.applicationservice.ApplicationService;
+
+
+public class TestNestedSearchQuery extends TestClient
+{
+	public static void main(String args[])
+	{
+		TestNestedSearchQuery client = new TestNestedSearchQuery();
+		try
+		{
+			client.testSearch();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void testSearch() throws Exception
+	{
+		ApplicationService appService = ApplicationServiceProvider.getApplicationService();
+
+		Card card1 = new Card();
+		card1.setId(6);
+		
+		Card card2 = new Card();
+		card2.setId(2);
+		
+		List<Card> cardCollection = new ArrayList<Card>();
+		cardCollection.add(card1);
+		cardCollection.add(card2);
+		
+		String path = "gov.nih.nci.cacoresdk.domain.other.levelassociation.Suit," +
+					  "gov.nih.nci.cacoresdk.domain.other.levelassociation.Card";
+		
+		Collection results = appService.search(path, cardCollection);
+		System.out.println("Number of qualifying records: " + results.size());
+		for(Object obj : results)
+		{
+			printObject(obj, Suit.class);
+		}
+	}
+}
