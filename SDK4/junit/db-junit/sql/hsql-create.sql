f4694e20a3f8fc98fa9f9da2562765830a36c246
U-NIH\konkapv <konkapv@mail.nih.gov>
2013-04-22 13:43:07 -0400

    Update copyright

--- SDK4/junit/db-junit/sql/hsql-create.sql
+++ SDK4/junit/db-junit/sql/hsql-create.sql
@@ -1,3 +1,11 @@
+/*L
+   Copyright Ekagra Software Technologies Ltd.
+   Copyright SAIC
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/cacore-sdk/LICENSE.txt for details.
+L*/
+
 create SCHEMA @DB-NAME@ AUTHORIZATION DBA;
 
 grant @DB-NAME@.* to @DB-USERNAME@@"%" identified by "@DB-PASSWORD@";
