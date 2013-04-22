334f6398c47c0e45c60166d8e7a88e5523a47cb3
U-NIH\konkapv <konkapv@mail.nih.gov>
2013-04-22 13:47:06 -0400

    Update copyright

--- conf/database-scripts/create_user.sql
+++ conf/database-scripts/create_user.sql
@@ -1,3 +1,11 @@
+/*L
+   Copyright Ekagra Software Technologies Ltd.
+   Copyright SAIC
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/cacore-sdk/LICENSE.txt for details.
+L*/
+
 grant all privileges on cacore.* to 'cabiouser'@'localhost' identified by 'cabio' with grant option;
 grant all privileges on cacore.* to 'cabiouser'@'localhost' identified by 'cabio' with grant option;
 grant all privileges on cacore.* to 'cabiouser'@'*' identified by 'cabio' with grant option;
