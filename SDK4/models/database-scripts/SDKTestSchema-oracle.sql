dd98be16e678e337b9cd63aa3e5f63cb09bebf2b
U-NIH\konkapv <konkapv@mail.nih.gov>
2013-04-22 13:44:23 -0400

    Update copyright

--- SDK4/models/database-scripts/SDKTestSchema-oracle.sql
+++ SDK4/models/database-scripts/SDKTestSchema-oracle.sql
@@ -1,3 +1,11 @@
+/*L
+   Copyright Ekagra Software Technologies Ltd.
+   Copyright SAIC
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/cacore-sdk/LICENSE.txt for details.
+L*/
+
 CREATE SEQUENCE CSM_APPLICATI_APPLICATION__SEQ
 	INCREMENT BY 1
 	START WITH 4
